5c80baf0e1a1fe3d5601d1eaa6b94bab68ce0915
Roger Lopez <73207365+Rllopez0410@users.noreply.github.com>
2023-01-12 16:50:58 -0500

    Update script.js
    created a new function for the delete button in addition to adding CSS into the script so that users are able to can visually identify what function they're currently using. Also, I corrected the the buttons and made the play button able to start and resume the audio by adding a flag variable.

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
 const textInput = document.getElementById('txt');
 const playAudio = document.getElementById('play');
-const PauseAudio = document.getElementById('pause');
-const resumeAudio = document.getElementById('resume');
+const stopAudio = document.getElementById('stop');
+const pauseAudio = document.getElementById('pause');
 const section = document.getElementById('content');
+const dltbtn = document.getElementById('delete');
+
+let a = '';
+let clickPause = false;
 
 section.addEventListener("scroll", function() {
     if (section.scrollTop >= 1) {
@@ -12,31 +16,57 @@ section.addEventListener("scroll", function() {
     }
 });
 
-let a = '';
-let click = false;
-
 playAudio.addEventListener('click', () => {
-    a = textInput.value;
-    speech(a);
-    console.log(a);
+    if (!clickPause) {
+        a = textInput.value;
+        speech(a);
+    } else if (clickPause) {
+        resumeSpeech();
+    }
+    document.getElementById('pause').style.color="black";
+    document.getElementById('play').style.color="red";
+    document.getElementById('stop').style.color="black";
 });
 
-resumeAudio.addEventListener('click', resumeSpeech);
+dltbtn.addEventListener('click', () => {
+    textInput.value = "";
+    document.getElementById('pause').style.color="black";
+    document.getElementById('play').style.color="black";
+    document.getElementById('stop').style.color="black";
+});
 
-PauseAudio.addEventListener('click', stopSpeech);
+pauseAudio.addEventListener('click', pauseSpeech);
+
+stopAudio.addEventListener('click', stopSpeech);
 
 function speech(text) {
-    const utterance = new SpeechSynthesisUtterance(text)
-    speechSynthesis.speak(utterance)
+    const utterance = new SpeechSynthesisUtterance(text);
+    speechSynthesis.speak(utterance);
+    //Create a condition that does not allow the user to click the play button
+    //again while the synthesis is speaking.
 }
 
 //Create a function that allows the user to pick the speed of the reader
 //if the user picks the value of 1 the speed should equal to slow.
 
-function stopSpeech() {
+function pauseSpeech() {
     speechSynthesis.pause();
+    clickPause = true;
+    document.getElementById('pause').style.color="red";
+    document.getElementById('play').style.color="black";
+    document.getElementById('stop').style.color="black";
 }
 
 function resumeSpeech() {
-    speechSynthesis.resume()
+    speechSynthesis.resume();
+    clickPause = false;
+}
+ 
+function stopSpeech() {
+    if (!clickPause || clickPause) {
+        speechSynthesis.cancel();
+        document.getElementById('pause').style.color="black";
+        document.getElementById('play').style.color="black";
+        document.getElementById('stop').style.color="red";
+    }
 }
